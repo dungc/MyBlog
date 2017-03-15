c8064d6eb6874e344267570d7dee76545fd215ce
APPLE <apple@APPLEs-MacBook-Pro.local>
2017-03-16 00:30:07 +0800

    完善 app.py

--- www/static/app.py
+++ www/static/app.py
@@ -1,6 +1,6 @@
 import logging
-logging.basicConfig(level=logging.INFO)
 import asyncio
+from asyncio import AbstractEventLoop
 import os
 import json
 import time
@@ -9,18 +9,21 @@ from datetime import datetime
 
 from aiohttp import web
 
+
 def index(request):
     return web.Response(headers={'Content-Type': 'text/html'}, body=b'<h1>Hello World</h1>')
 
-async def init(loop):
-    app = web.Application(loop=loop)
+
+async def init(event_loop: AbstractEventLoop):
+    app = web.Application(loop=event_loop)
     app.router.add_route('GET', '/', index)
     port = 9000
-    srv = await loop.create_server(app.make_handler(), '127.0.0.1', port=port)
+    srv = await event_loop.create_server(app.make_handler(), '127.0.0.1', port=port)
     info = 'Server started at http://127.0.0.1:%d' % 9000
     logging.info(info)
     return srv
 
+logging.basicConfig(level=logging.INFO)
 loop = asyncio.get_event_loop()
 loop.run_until_complete(init(loop))
 loop.run_forever()
