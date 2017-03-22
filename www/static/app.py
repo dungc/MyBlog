b567e7d4d972092b18ef0aa97476431ef5f59bb5
APPLE <cd529723548@163.com>
2017-03-22 23:50:32 +0800

    测试基本功能。

--- www/static/app.py
+++ www/static/app.py
@@ -8,9 +8,10 @@ import time
 from datetime import datetime
 
 from aiohttp import web
+from aiohttp import Request
 
 
-def index(request):
+def index(request: Request):
     return web.Response(headers={'Content-Type': 'text/html'}, body=b'<h1>Hello World</h1>')
 
 
@@ -18,7 +19,7 @@ async def init(event_loop: AbstractEventLoop):
     app = web.Application(loop=event_loop)
     app.router.add_route('GET', '/', index)
     port = 9000
-    srv = await event_loop.create_server(app.make_handler(), '127.0.0.1', port=port)
+    srv = await event_loop.create_server(app.make_handler(), '', port=port)
     info = 'Server started at http://127.0.0.1:%d' % 9000
     logging.info(info)
     return srv
